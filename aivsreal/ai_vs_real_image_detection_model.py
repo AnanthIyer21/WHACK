7ef5c1acad90b59532e66352ad786ce2e48f7fe1
AnanthIyer21 <158300833+AnanthIyer21@users.noreply.github.com>
2025-10-18 12:54:06 +0000

    ai edits

--- aivsreal/ai_vs_real_image_detection_model.py
+++ aivsreal/ai_vs_real_image_detection_model.py
@@ -6,4 +6,4 @@ client = InferenceClient(
     api_key=os.environ["HF_TOKEN"],
 )
 
-output = client.image_classification("cats.jpg", model="dima806/ai_vs_real_image_detection")
+output = client.image_classification("test.jpg", model="dima806/ai_vs_real_image_detection")
