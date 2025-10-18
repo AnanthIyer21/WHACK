dca70020da27ac336adb778bcdc11b33dcb136f1
AnanthIyer21 <158300833+AnanthIyer21@users.noreply.github.com>
2025-10-18 12:24:03 +0000

    ai vs real

---
+++ aivsreal/ai vs real image detection model.py
@@ -0,0 +1,9 @@
+import os
+from huggingface_hub import InferenceClient
+
+client = InferenceClient(
+    provider="hf-inference",
+    api_key=os.environ["HF_TOKEN"],
+)
+
+output = client.image_classification("cats.jpg", model="dima806/ai_vs_real_image_detection")
