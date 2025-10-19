879a95635194af95879badba2ee365fc1f359852
jjy0404 <jiyongjung04@gmail.com>
2025-10-19 08:06:06 +0000

    version4

--- extension3/background.js
+++ extension3/background.js
@@ -1,6 +1,6 @@
 // Hugging Face API Inference Endpoint Configuration
 const HF_API_URL = 'https://api-inference.huggingface.co/models/';
-const DEEPFAKE_MODEL = 'Ateeqq/ai-vs-human-image-detector';
+const DEEPFAKE_MODEL = 'haywoodsloan/ai-image-detector-deploy';
 
 /**
  * Retrieves the Hugging Face API token from local storage.
